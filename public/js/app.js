aed76ef0161c2cae362e018ac1f6475b7acdd992
Gekter <vorobev.slava.17.01.02@mail.ru>
2022-05-27 22:19:25 +0800

    websocket first try

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+// const status1 = document.getElementById('status');
+// const messages = document.getElementById('messages');
+// const form = document.getElementById('form');
+// const input = document.getElementById('input');
+
+// const ws = new WebSocket('ws://localhost:3000')
+
+// function setStatus(value) {
+//     status1.innerHTML = value;
+// }
+
+// function printMessage(value) {
+//     const li = document.createElement('li');
+
+//     li.innerHTML = value;
+//     messages.appendChild(li);
+// }
+
+// ws.onopen = () => setStatus('Online');
+
+// ws.onclose = () => setStatus('Disconnect');
+
+// ws.onmessage = response => printMessage(response.data);
+
+
